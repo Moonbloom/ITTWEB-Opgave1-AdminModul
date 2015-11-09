5c4b9e85ef2e80eb055073795c5f34d0bf818d42
CSO <Chr.sorensen1990@gmail.com>
2015-11-09 16:49:11 +0100

    Views mm

---
+++ ITTWEB-Opg1-AdminModul/Controllers/ComponenttypeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ITTWEB_Opg1_AdminModul.Controllers
+{
+  public class ComponenttypeController : Controller
+  {
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
