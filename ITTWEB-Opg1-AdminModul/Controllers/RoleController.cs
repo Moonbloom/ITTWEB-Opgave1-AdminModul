7a7b7912b8f9abd0048591af7a662b79d98b6134
CSO <Chr.sorensen1990@gmail.com>
2015-11-15 16:34:51 +0100

    Stuff

--- ITTWEB-Opg1-AdminModul/Controllers/RoleController.cs
+++ ITTWEB-Opg1-AdminModul/Controllers/RoleController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ITTWEB_Opg1_AdminModul.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ITTWEB_Opg1_AdminModul.Controllers
@@ -11,15 +12,102 @@ namespace ITTWEB_Opg1_AdminModul.Controllers
   public class RoleController : Controller
   {
     ApplicationDbContext context = new ApplicationDbContext();
-    //
-    // GET: /Role/
-    public ActionResult Index()
-    {
-      var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
-      ViewBag.Roles = list;
-      return View();
+        
+        //
+        // POST: /Roles/Edit/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                context.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Roles/Delete/5
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = context.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            context.Roles.Remove(thisRole);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ManageUserRoles()
+        {
+            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            return View(list);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        {
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            var account = new AccountController();
+            account.UserManager.AddToRole(user.Id, RoleName);
+            
+            ViewBag.ResultMessage = "Role created successfully !";
+            
+            // prepopulat roles for the view dropdown
+            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;   
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string UserName)
+        {            
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+                var account = new AccountController();
+
+                ViewBag.RolesForThisUser = account.UserManager.GetRoles(user.Id);
+
+                // prepopulat roles for the view dropdown
+                var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = list;            
+            }
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleForUser(string UserName, string RoleName)
+        {
+            var account = new AccountController();
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            if (account.UserManager.IsInRole(user.Id, RoleName))  
+            {
+                account.UserManager.RemoveFromRole(user.Id, RoleName);
+                ViewBag.ResultMessage = "Role removed from this user successfully !";
+            }
+            else
+            {
+                ViewBag.ResultMessage = "This user doesn't belong to selected role.";
+            }
+            // prepopulat roles for the view dropdown
+            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("ManageUserRoles");
+        }
     }
-  }
+  
   public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
   {
     public ApplicationDbContext()
--- ITTWEB-Opg1-AdminModul/Controllers/StudentController.cs
+++ ITTWEB-Opg1-AdminModul/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using ITTWEB_Opg1_AdminModul.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace ITTWEB_Opg1_AdminModul.Controllers
 {
@@ -7,13 +9,18 @@ namespace ITTWEB_Opg1_AdminModul.Controllers
   {
     public ActionResult Index()
     {
-      var testData = new Student()
+      var testData = new List<Student>()
       {
-        Email = "Nope@nopemail.com",
-        MobilNo = "666",
-        StudentName = "Poul",
-        StudentId = "1",
+        new Student()
+        {
+            Email = "Nope@nopemail.com",
+            MobilNo = "666",
+            StudentName = "Poul",
+            StudentId = "1",    
+        }
       };
+
+
       return View(testData);
     }
   }
