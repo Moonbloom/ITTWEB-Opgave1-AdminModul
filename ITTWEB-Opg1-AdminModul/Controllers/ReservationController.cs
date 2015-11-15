2a4baff762bb6dfb96d8dede3a33a12ac1ea4588
CSO <Chr.sorensen1990@gmail.com>
2015-11-15 13:00:07 +0100

    Test data added to controllers

--- ITTWEB-Opg1-AdminModul/Controllers/ReservationController.cs
+++ ITTWEB-Opg1-AdminModul/Controllers/ReservationController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ITTWEB_Opg1_AdminModul.Models;
 
 namespace ITTWEB_Opg1_AdminModul.Controllers
 {
@@ -6,7 +9,19 @@ namespace ITTWEB_Opg1_AdminModul.Controllers
   {
     public ActionResult Index()
     {
-      return View();
+        var testData = new List<LoanInformation>();
+      testData.Add(new LoanInformation(
+      {
+       Component = new Component(){ComponentType = new ComponentType(){ComponentName = "Kat"}},
+       Id = 1,
+       IsEmailSend = false,
+       IsReservation = true,
+       LoanDate = new DateTime(2015,11,15),
+       ReservationDate = new DateTime(2015,11,10),
+       ReturnDate = new DateTime(2016,11,11),
+       Student = new Student(){StudentName = "Poul"}       
+      });
+      return View(testData);
     }
   }
 }
