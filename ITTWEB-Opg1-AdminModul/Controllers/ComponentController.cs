4043fe779a65001910ce6c911b9b457c777b84ea
CSO <Chr.sorensen1990@gmail.com>
2015-11-15 12:35:08 +0100

    Added testdata + structure

--- ITTWEB-Opg1-AdminModul/Controllers/ComponentController.cs
+++ ITTWEB-Opg1-AdminModul/Controllers/ComponentController.cs
@@ -3,16 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ITTWEB_Opg1_AdminModul.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace ITTWEB_Opg1_AdminModul.Controllers
 {
-    public class ComponentController : Controller
+  public class ComponentController : Controller
+  {
+
+    //
+    // GET: /Component/
+    public ActionResult Index()
     {
-        //
-        // GET: /Component/
-        public ActionResult Index()
-        {
-            return View();
-        }
-	}
+      var testData = new List<Component>();
+      var com1 = new Component()
+      {
+        AdminComment = "admin comment",
+        ComponentNumber = 1337,
+        ComponentType = new ComponentType(){ComponentName = "namename"},
+        Id = 1,
+        LoanInformation = null,
+        SerieNr = "1337",
+        UserComment = "this product sux"
+      };
+      var com2 = new Component()
+      {
+        AdminComment = "admin comment2",
+        ComponentNumber = 1338,
+        ComponentType = new ComponentType(){LocalImageUrl = "http://dreamatico.com/data_images/kitten/kitten-1.jpg",ComponentName = "Kat"},
+        Id = 2,
+        LoanInformation = null,
+        SerieNr = "1338",
+        UserComment = "this product sux also"
+      };
+      var com3 = new Component()
+      {
+        AdminComment = "admin comment3",
+        ComponentNumber = 1339,
+        ComponentType = new ComponentType() {ComponentName = "Kat" },
+        Id = 3,
+        LoanInformation = null,
+        SerieNr = "1339",
+        UserComment = "this product sux also 1 "
+      };
+      testData.Add(com1);
+      testData.Add(com2);
+      testData.Add(com3);
+      return View(testData);
+    }
+  }
 }
--- ITTWEB-Opg1-AdminModul/Controllers/ComponenttypeController.cs
+++ ITTWEB-Opg1-AdminModul/Controllers/ComponenttypeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ITTWEB_Opg1_AdminModul.Models;
+using Microsoft.Owin.Security.Google;
 
 namespace ITTWEB_Opg1_AdminModul.Controllers
 {
@@ -7,7 +10,21 @@ namespace ITTWEB_Opg1_AdminModul.Controllers
 
     public ActionResult Index()
     {
-      return View();
+      var testData = new List<ComponentType>();
+      var com1 = new ComponentType()
+      {
+        Category = new Category() { CategoryName = "Dyr"},
+        CategoryId = 1,
+        ComponentInfo = "this is cat",
+        Datasheet = "https://www.google.dk/", 
+        ComponentName = "kat",
+        ComponentId = 1337,
+        LocalImageUrl = "https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg",
+        ManufacturerLink = "https://www.google.dk/",
+      };
+
+      testData.Add(com1);
+      return View(testData);
     }
   }
 }
