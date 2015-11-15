f3a14144cefb64960134188515ad3e6cca9974b1
Kasper Liholm <kasperliholm@hotmail.com>
2015-11-15 12:57:08 +0100

    made it prettier

--- ITTWEB-Opg1-AdminModul/Models/ComponentType.cs
+++ ITTWEB-Opg1-AdminModul/Models/ComponentType.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Data.Entity.ModelConfiguration.Configuration;
+﻿using System.Collections.Generic;
 
 namespace ITTWEB_Opg1_AdminModul.Models
 {
     public class ComponentType
     {
         public int Id { get; set; }
-        public String ComponentName { get; set; }
-        public String ComponentInfo { get; set; }
-        public String Datasheet { get; set; }
-        public String LocalImageUrl { get; set; }
-        public String ManufacturerLink { get; set; }
-
-        public int ComponentId { get; set; }
-        public virtual Component Component { get; set; }
+        public string ComponentName { get; set; }
+        public string ComponentInfo { get; set; }
+        public string Datasheet { get; set; }
+        public string LocalImageUrl { get; set; }
+        public string ManufacturerLink { get; set; }
+        
+        public virtual IEnumerable<Component> Component { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
     }
