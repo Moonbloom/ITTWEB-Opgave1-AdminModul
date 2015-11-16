6e577d6503507b255b2412700a5c7ea50fa671c8
Kasper Liholm <kasperliholm@hotmail.com>
2015-11-16 16:13:33 +0100

    moved navbar to own file added sass

--- ITTWEB-Opg1-AdminModul/Models/Component.cs
+++ ITTWEB-Opg1-AdminModul/Models/Component.cs
@@ -1,15 +1,23 @@
-﻿namespace ITTWEB_Opg1_AdminModul.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITTWEB_Opg1_AdminModul.Models
 {
     public class Component
     {
         public int Id { get; set; }
+        [Display(Name = "Component number")]
         public int ComponentNumber { get; set; }
+        [Display(Name = "Serie number")]
         public string SerieNr { get; set; }
+        [Display(Name = "Admin comment")]
         public string AdminComment { get; set; }
+        [Display(Name = "User comment")]
         public string UserComment { get; set; }
 
+        [Display(Name = "Component type id")]
         public int ComponentTypeId { get; set; }
         public virtual ComponentType ComponentType { get; set; }
+        [Display(Name = "Loan id")]
         public int? LoanInformationId { get; set; }
         public virtual LoanInformation LoanInformation { get; set; }
     }
--- ITTWEB-Opg1-AdminModul/Models/Student.cs
+++ ITTWEB-Opg1-AdminModul/Models/Student.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ITTWEB_Opg1_AdminModul.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [Display(Name = "Mobile number")]
         public string MobilNo { get; set; }
+        [Display(Name = "Student id")]
         public string StudentId { get; set; }
+        [Display(Name = "Student name")]
         public string StudentName { get; set; }
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Display(Name = "Loan id")]
         public int LoanInformationId { get; set; }
         public virtual IEnumerable<LoanInformation> LoanInformations { get; set; }
     }
