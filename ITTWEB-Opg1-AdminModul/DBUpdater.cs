069366743c004887b0ac187627b007adea77eaa6
Kasper Liholm <kasperliholm@hotmail.com>
2015-11-16 15:20:48 +0100

    more fixes & sass

--- ITTWEB-Opg1-AdminModul/DBUpdater.cs
+++ ITTWEB-Opg1-AdminModul/DBUpdater.cs
@@ -1,4 +1,5 @@
-﻿using ITTWEB_Opg1_AdminModul.Models;
+﻿using System;
+using ITTWEB_Opg1_AdminModul.Models;
 
 namespace ITTWEB_Opg1_AdminModul
 {
@@ -25,9 +26,28 @@ namespace ITTWEB_Opg1_AdminModul
                 ComponentTypeId = comType1.Id,
                 SerieNr = "1337",
                 UserComment = "this product sux",
-                AdminComment = "admin comment"
+                AdminComment = "admin comment",
             };
             context.Components.Add(com1);
+
+            var student1 = new Student
+            {
+                Email = "Nope@gmail.com",
+                MobilNo = "88888888",
+                StudentName = "Per",
+                StudentId = "201012345",
+            };
+            context.Students.Add(student1);
+
+            var loan1 = new LoanInformation
+            {
+                IsReservation = false,
+                LoanDate = DateTime.Now,
+                StudentId = student1.Id,
+                ReservationDate = DateTime.Now,
+                ReturnDate = DateTime.Now.AddMonths(1)
+            };
+            context.LoanInformations.Add(loan1);
 
             context.SaveChanges();
         }
--- ITTWEB-Opg1-AdminModul/Models/LoanInformation.cs
+++ ITTWEB-Opg1-AdminModul/Models/LoanInformation.cs
@@ -5,11 +5,11 @@ namespace ITTWEB_Opg1_AdminModul.Models
     public class LoanInformation
     {
         public int Id { get; set; }
-        public DateTime LoanDate { get; set; }
+        public DateTime? LoanDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public bool IsEmailSend { get; set; }
         public bool IsReservation { get; set; }
-        public DateTime ReservationDate { get; set; }
+        public DateTime? ReservationDate { get; set; }
         
         public int StudentId { get; set; }
         public virtual Student Student { get; set; }
